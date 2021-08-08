2c0f6d789e269f5b045ea9a5fe67196d285e09d6
Rifdar <assasinnyx@mail.ru>
2021-08-08 17:46:27 +0300

    minority changes with docker + changed generation strategy of Group

--- src/main/java/ru/softwave/pool/model/entity/Group.java
+++ src/main/java/ru/softwave/pool/model/entity/Group.java
@@ -22,7 +22,7 @@ import java.util.Set;
 public class Group {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column private Time startTime;
